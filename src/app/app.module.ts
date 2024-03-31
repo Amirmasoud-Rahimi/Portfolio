4454d76ab261683313bcfb882f597c7031f6a172
Amirmasoud-Rahimi <amirmasoudrahimiofficial@gmail.com>
2024-03-31 13:30:06 +0330

    portfolio website

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,9 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {TypingAnimatorModule} from "angular-typing-animator";
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TypingAnimatorModule
   ],
   providers: [],
   bootstrap: [AppComponent]
